258d66298104dc5ba0639f023de0e891afe0031e
jonathanbataire <jonathanbataire@gmail.com>
2020-09-11 17:36:07 +0300

    get data

--- app/src/main/java/com/example/gadsleaderboard/LearningLeadersFragment.java
+++ app/src/main/java/com/example/gadsleaderboard/LearningLeadersFragment.java
@@ -1,6 +1,7 @@
 package com.example.gadsleaderboard;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +12,23 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
 
 public class LearningLeadersFragment extends Fragment {
 
     private PerformanceRecyclerAdapter adapter;
+    public static final String BASE_URL = "https://gadsapi.herokuapp.com/";
 
     @Override
     public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -33,5 +46,34 @@ public class LearningLeadersFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         adapter = new PerformanceRecyclerAdapter(getActivity(), Names);
         recyclerView.setAdapter(adapter);
+        getData(view);
     }
+
+    public void getData (final View view){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        GetAPIService service = retrofit.create(GetAPIService.class);
+        Call<JsonArray> jsonCall = service.getContacts();
+        jsonCall.enqueue(new Callback<JsonArray>() {
+            @Override
+            public void onResponse(Call<JsonArray> call, Response<JsonArray> response) {
+                Toast.makeText(view.getContext(),response.body().toString(), Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onFailure(Call<JsonArray> call, Throwable t) {
+                Toast.makeText(view.getContext(),"nope"+ t, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public interface GetAPIService {
+        @GET("/api/hours")
+        Call<JsonArray> getContacts();
+    }
+
 }
